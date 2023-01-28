afb4f1343351e317956e5f9e5b2221da9c7aea8a
Felix Anton <feanbe@t-online.de>
2023-01-28 18:33:40 +0100

    environ installiert und in settings hinzugefügt

--- felixblog/settings/common.py
+++ felixblog/settings/common.py
@@ -10,8 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import environ
 import os
 from pathlib import Path
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
