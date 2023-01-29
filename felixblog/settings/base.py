f52b08f6c9d7529fe7858286f7a3571a42d5b2c1
Felix Anton <feanbe@t-online.de>
2023-01-29 12:27:40 +0100

    entfernen dbport prod

--- felixblog/settings/base.py
+++ felixblog/settings/base.py
@@ -75,22 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'felixblog.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.environ['DBNAME'],
-       'USER': os.environ['DBUSER'],
-       'PASSWORD': os.environ['DBPASS'],
-       'HOST': os.environ['DBHOST'],
-       'PORT': os.environ['DB_PORT'],
-   }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
--- felixblog/settings/development.py
+++ felixblog/settings/development.py
@@ -4,3 +4,17 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.environ['DBNAME'],
+       'USER': os.environ['DBUSER'],
+       'PASSWORD': os.environ['DBPASS'],
+       'HOST': os.environ['DBHOST'],
+       'PORT': os.environ['DB_PORT'],
+   }
+}
