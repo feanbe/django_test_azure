2362ba4eecbb002682569db07fd2405f4fd5ab94
Felix Anton <feanbe@t-online.de>
2023-01-29 17:58:21 +0100

    anpassen csrf token

--- felixblog/settings/production.py
+++ felixblog/settings/production.py
@@ -5,7 +5,8 @@ import os
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
