088944423a42b31f1effbdbd70fd13d5a5d74762
Felix Anton <feanbe@t-online.de>
2023-01-29 17:46:43 +0100

    csrf azure test

--- felixblog/settings/production.py
+++ felixblog/settings/production.py
@@ -4,7 +4,8 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
